815d067a2341c16399794e36cdaed6036792c6c3
hansang <pokbw3720@gmail.com>
2021-05-26 10:04:27 +0900

    gitignore 설정

---
+++ team_sub_prj/src/kr/co/sist/dao/LoginVO.java
@@ -0,0 +1,34 @@
+package kr.co.sist.dao;
+
+public class LoginVO {
+	private String id,pass;
+	
+	public LoginVO() {
+		
+	}
+
+	public LoginVO(String id, String pass) {
+		super();
+		this.id = id;
+		this.pass = pass;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+	
+}
